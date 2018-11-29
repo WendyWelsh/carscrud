2652b57f68ec3a293adb90545c6ac04fa9be690d
Wendy <mac@Wendys-MacBook-Pro-1155.local>
2018-11-29 10:48:15 -0700

    begin

--- cars.js
+++ cars.js
@@ -1,1 +1,8 @@
-// hello
+// hello PREP (parameters, return, example, pseudocode)
+//create react js doc - setup imports
+//make an object that has cars in it (color, make and model)
+//CRUD create, read, update, delete
+// first make the read of CRUD
+// second make delete
+// third make create
+// forth make update
